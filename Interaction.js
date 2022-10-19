a669b364ce17daa955a12b2a3f62cdd8a9b26f73
lipey <100057557+lipey1@users.noreply.github.com>
2022-10-18 23:19:46 -0300

    Create Interaction.js

---
+++ Interaction.js
@@ -0,0 +1,40 @@
+// Use o código abaixo no seu InteractionCreate
+// Caso tenha algum erro no código entre em contato com lipeyzin#0001
+
+        if (interaction.guild) {
+
+            let finalRoles;
+            let roles = interaction.guild.roles.cache.forEach(async (role) => {
+
+                let cargos = interaction.guild.roles.cache.get(role.id)
+
+
+                if (cargos.permissions.toArray().includes('Administrator') && cargos.members.find(m => m.user.id === client.user.id) !== undefined) {
+
+                    finalRoles = cargos
+
+                }
+
+                return
+            })
+
+
+            if (finalRoles === undefined) {
+
+                const bot_link = new Discord.ActionRowBuilder().addComponents([
+                    new Discord.ButtonBuilder()
+                        .setStyle(Discord.ButtonStyle.Link)
+                        .setURL(`https://discord.com/api/oauth2/authorize?client_id=969301039515852880&permissions=8&scope=bot%20applications.commands`)
+                        .setLabel("Convidar")
+                        .setDisabled(false)
+                ]);
+
+                const embed_norole = new Discord.EmbedBuilder()
+                    .setDescription(`❌  *Eu não possuo nenhum cargo com a permissão de **Administrador**, caso queira criar um meu, que vem por padrão junto a mim, me retire do servidor e me coloque novamente usando o botão abaixo, lembre-se de deixar a permissão de **Administrador***`)
+                    .setColor("#2f3136")
+
+                return interaction.reply({ embeds: [embed_norole], components: [bot_link], ephemeral: true });
+
+            }
+
+        }
